850f9d5c631c3c487ba8eb9cb5f545ddced783db
theobarbier16-gif <theo.barbier16@gmail.com>
2025-08-25 23:12:54 +0200

    Recréer les fichiers principaux pour corriger l'écran blanc

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  console.error('Root element not found');
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
